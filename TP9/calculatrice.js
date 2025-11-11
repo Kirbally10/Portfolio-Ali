b54cd790718bea95baf79930245d7b1dc8cad3d6
Kirbally10 <gaali.srour@gmail.com>
2025-11-11 20:43:44 +0100

    Update calculatrice.js

--- TP9/calculatrice.js
+++ TP9/calculatrice.js
@@ -1,29 +1,25 @@
-function somme()
-{
+function somme()    {
     var a = document.getElementById("t1").value;
     var b = document.getElementById("t2").value;
     var c = Number(a) + Number(b);
     document.getElementById("résultat").value = c;
 }
 
-function somme()
-{
+function somme()    {
     var a = document.getElementById("t1").value;
     var b = document.getElementById("t2").value;
     var c = Number(a) + Number(b);
     document.getElementById("résultat").value = c;
 }
 
-function somme()
-{
+function somme()    {
     var a = document.getElementById("t1").value;
     var b = document.getElementById("t2").value;
     var c = Number(a) + Number(b);
     document.getElementById("résultat").value = c;
 }
 
-function somme()
-{
+function somme()    {
     var a = document.getElementById("t1").value;
     var b = document.getElementById("t2").value;
     var c = Number(a) + Number(b);
